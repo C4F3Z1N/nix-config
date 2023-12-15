27d2be1928456e48a20de3629d5f9c5cdd7fe150
João Nogueira <joao.nogueira@eivee.io>
2023-12-15 19:22:15 +0100

    Initial commit

---
+++ hosts/climbing-foxhound/disko-config.nix
@@ -0,0 +1,108 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/disk/by-id/nvme-eui.ace42e000a9f5cf42ee4ac0000000001";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02";
+            };
+
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "defaults"
+                ];
+              };
+            };
+
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                settings.allowDiscards = true;
+                passwordFile = "/tmp/secret.key";
+                content = {
+                  type = "zfs";
+                  pool = "zroot";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+
+    zpool = {
+      zroot = {
+        type = "zpool";
+        rootFsOptions = {
+          acltype = "posixacl";
+          canmount = "off";
+          compression = "zstd";
+          dnodesize = "auto";
+          normalization = "formD";
+          relatime = "on";
+          xattr = "sa";
+        };
+
+        datasets = {
+          ephemeral = {
+            type = "zfs_fs";
+            options = {
+              canmount = "off";
+              mountpoint = "none";
+            };
+          };
+
+          persistent = {
+            type = "zfs_fs";
+            options = {
+              canmount = "off";
+              mountpoint = "none";
+            };
+          };
+
+          "ephemeral/root" = {
+            type = "zfs_fs";
+            mountpoint = "/";
+            options."com.sun:auto-snapshot" = "false";
+            postCreateHook = "zfs snapshot zroot/ephemeral/root@blank";
+          };
+
+          "persistent/nix" = {
+            type = "zfs_fs";
+            mountpoint = "/nix";
+            options."com.sun:auto-snapshot" = "false";
+          };
+
+          "persistent/stuff" = {
+            type = "zfs_fs";
+            mountpoint = "/persistent";
+            options."com.sun:auto-snapshot" = "false";
+          };
+
+          swap = {
+            type = "zfs_volume";
+            size = "17G";
+            content = {
+              type = "swap";
+              resumeDevice = true;
+            };
+          };
+        };
+      };
+    };
+  };
+}
+

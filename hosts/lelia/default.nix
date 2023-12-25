17dda72093f04befda3056a5dda37ab2c0d412a1
João Nogueira <joao.nogueira@eivee.io>
2023-12-25 23:32:06 +0100

    3/3 consolidate host configuration

--- hosts/lelia/default.nix
+++ hosts/lelia/default.nix
@@ -5,119 +5,136 @@
   config,
   inputs,
   lib,
+  modulesPath,
   pkgs,
   ...
 }: {
   imports = with inputs; [
     # Include the results of the hardware scan.
     ./disko-config.nix
-    ./hardware-configuration.nix
+    (modulesPath + "/installer/scan/not-detected.nix")
     disko.nixosModules.disko
     hardware.nixosModules.lenovo-thinkpad-t14s-amd-gen1
     impermanence.nixosModules.impermanence
   ];
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.efiInstallAsRemovable = true;
+  boot = {
+    loader.grub = {
+      enable = true;
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+    };
 
-  boot.initrd.postDeviceCommands = lib.mkAfter "zfs rollback -r zroot/ephemeral/root@blank";
+    initrd = {
+      availableKernelModules = ["nvme" "ehci_pci" "xhci_pci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc"];
+      kernelModules = [];
+      postDeviceCommands = lib.mkAfter "zfs rollback -r zroot/ephemeral/root@blank";
+    };
 
-  # boot.initrd.systemd = {
-  #   enable = true;
-  #   services.rollback = {
-  #     description = "Rollback ZFS datasets to a pristine state";
-  #     wantedBy = [
-  #       "initrd.target"
-  #     ];
-  #     after = [
-  #       "zfs-import-zroot.service"
-  #     ];
-  #     before = [
-  #       "sysroot.mount"
-  #     ];
-  #     path = with pkgs; [
-  #       zfs
-  #     ];
-  #     unitConfig.DefaultDependencies = "no";
-  #     serviceConfig.Type = "oneshot";
-  #     script = "zfs rollback -r zroot/ephemeral/root@blank";
-  #   };
-  # };
+    kernelModules = ["kvm-amd"];
+    extraModulePackages = [];
+  };
 
-  # boot.kernelParams = [ "quiet" ];
-  # boot.plymouth.enable = true;
+  hardware = {
+    cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    pulseaudio.enable = false;
+  };
 
-  # boot.initrd.luks.devices."luks-0c5de71c-d8df-4a6c-9114-9df2cb1f2b99".device = "/dev/disk/by-uuid/0c5de71c-d8df-4a6c-9114-9df2cb1f2b99";
-  networking.hostName = lib.mkDefault (builtins.baseNameOf ./.); # Define your hostname.
-  networking.hostId = "8425e349";
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking = {
+    hostName = lib.mkDefault (builtins.baseNameOf ./.);
+    hostId = "8425e349";
+    useDHCP = lib.mkDefault true;
+    networkmanager.enable = true;
+  };
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  sound.enable = true;
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+  security = {
+    rtkit.enable = true;
+    sudo.extraConfig = ''
+      # rollback results in sudo lectures after each reboot
+      Defaults lecture = never
+    '';
+  };
 
-  # Set your time zone.
   time.timeZone = "Europe/Copenhagen";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_DK.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "da_DK.UTF-8";
-    LC_IDENTIFICATION = "da_DK.UTF-8";
-    LC_MEASUREMENT = "da_DK.UTF-8";
-    LC_MONETARY = "da_DK.UTF-8";
-    LC_NAME = "da_DK.UTF-8";
-    LC_NUMERIC = "da_DK.UTF-8";
-    LC_PAPER = "da_DK.UTF-8";
-    LC_TELEPHONE = "da_DK.UTF-8";
-    LC_TIME = "da_DK.UTF-8";
+  i18n = {
+    defaultLocale = "en_DK.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "da_DK.UTF-8";
+      LC_IDENTIFICATION = "da_DK.UTF-8";
+      LC_MEASUREMENT = "da_DK.UTF-8";
+      LC_MONETARY = "da_DK.UTF-8";
+      LC_NAME = "da_DK.UTF-8";
+      LC_NUMERIC = "da_DK.UTF-8";
+      LC_PAPER = "da_DK.UTF-8";
+      LC_TELEPHONE = "da_DK.UTF-8";
+      LC_TIME = "da_DK.UTF-8";
+    };
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "dk";
-    xkbVariant = "";
-  };
-
-  # Configure console keymap
   console.keyMap = "dk-latin1";
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+  services = {
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
 
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    xserver = {
+      enable = true;
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+      layout = "dk";
+      xkbVariant = "";
+    };
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    openssh = {
+      enable = true;
+      hostKeys = [
+        {
+          path = "/persistent/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+      ];
+    };
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  environment = {
+    systemPackages = with pkgs; [
+    ];
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+    persistence."/persistent" = {
+      directories = [
+        "/etc/NetworkManager/system-connections"
+        "/etc/nixos"
+        "/var/lib/bluetooth"
+        "/var/lib/nixos"
+        "/var/lib/systemd/coredump"
+        "/var/log"
+        {
+          directory = "/home/joao";
+          user = "joao";
+          group = "users";
+          mode = "0700";
+        }
+      ];
+      files = [
+        "/etc/machine-id"
+      ];
+    };
+  };
+
+  fileSystems."/persistent".neededForBoot = true;
+
+  nixpkgs = {
+    hostPlatform = lib.mkDefault "x86_64-linux";
+    config.allowUnfree = true;
+  };
+
   users.users.joao = {
     isNormalUser = true;
     description = "João";
@@ -129,80 +146,5 @@
     hashedPassword = "$y$j9T$Oh.cj23V4oPosds0kR12p/$Dq18ZR07MmeAHvw1UxMjmd1wWnIbpwyYAaIX.nZ8h69";
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    hostKeys = [
-      {
-        path = "/persistent/etc/ssh/ssh_host_ed25519_key";
-        type = "ed25519";
-      }
-      {
-        path = "/persistent/etc/ssh/ssh_host_rsa_key";
-        type = "rsa";
-        bits = 4096;
-      }
-    ];
-  };
-
-  fileSystems."/persistent".neededForBoot = true;
-
-  environment.persistence."/persistent" = {
-    directories = [
-      "/etc/NetworkManager/system-connections"
-      "/etc/nixos"
-      "/var/lib/bluetooth"
-      "/var/lib/nixos"
-      "/var/lib/systemd/coredump"
-      "/var/log"
-      {
-        directory = "/home/joao";
-        user = "joao";
-        group = "users";
-        mode = "0700";
-      }
-    ];
-    files = [
-      "/etc/machine-id"
-    ];
-  };
-
-  security.sudo.extraConfig = ''
-    # rollback results in sudo lectures after each reboot
-    Defaults lecture = never
-  '';
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "23.11";
 }

9f479d0d19a9378a38393e6d106a641aaa60ce0c
João Nogueira <joao.nogueira@eivee.io>
2023-12-27 12:30:29 +0100

    Started using flake-parts

--- flake.nix
+++ flake.nix
@@ -16,27 +16,33 @@
     wayland-pkgs.url = "github:nix-community/nixpkgs-wayland";
     wayland-pkgs.inputs.nixpkgs.follows = "nixpkgs";
 
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
     stable-pkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     unstable-pkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = inputs @ {self, ...}: let
-    inherit (inputs.nixpkgs) lib;
+    lib = with inputs; (nixpkgs.lib // flake-parts.lib);
     readDir' = path:
       lib.mapAttrs (found: _: path + "/${found}") (builtins.readDir path);
-  in rec {
-    inherit lib;
+  in
+    lib.mkFlake {inherit inputs;} {
+      flake = {
+        inherit lib;
 
-    nixosConfigurations = lib.mapAttrs (hostName: directory:
-      lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [directory];
-      }) (readDir' ./hosts);
+        nixosConfigurations = lib.mapAttrs (hostName: modulePath:
+          lib.nixosSystem {
+            specialArgs = {inherit inputs;};
+            modules = [modulePath];
+          }) (readDir' ./hosts);
+      };
 
-    formatter = lib.mapAttrs' (_: nixosSystem: let
-      inherit (nixosSystem) pkgs;
-    in
-      lib.nameValuePair pkgs.system pkgs.alejandra)
-    nixosConfigurations;
-  };
+      perSystem = {pkgs, ...}: {
+        formatter = pkgs.alejandra;
+      };
+
+      systems = ["aarch64-linux" "x86_64-linux"];
+    };
 }

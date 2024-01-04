02aa65a6478e75b3f12e489cc96a0aecc198a4d1
João Nogueira <joao.nogueira@eivee.io>
2024-01-04 15:54:04 +0100

    Added RSA host key

--- hosts/lelia/default.nix
+++ hosts/lelia/default.nix
@@ -97,10 +97,16 @@
 
     openssh = {
       enable = true;
-      hostKeys = [{
-        path = "/keep/etc/ssh/ssh_host_ed25519_key";
-        type = "ed25519";
-      }];
+      hostKeys = [
+        {
+          path = "/keep/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+        {
+          path = "/keep/etc/ssh/ssh_host_rsa_key";
+          type = "rsa";
+        }
+      ];
     };
   };
 

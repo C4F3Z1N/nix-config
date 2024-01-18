7fde5d98490e4cf98d0eed848d7befc8627623d4
João Nogueira <joao.nogueira@eivee.io>
2024-01-18 13:00:07 +0100

    Removed comments

--- hosts/lelia/default.nix
+++ hosts/lelia/default.nix
@@ -1,6 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 { config, inputs, lib, modulesPath, pkgs, ... }: {
   imports = with inputs; [
     ../../common/hosts

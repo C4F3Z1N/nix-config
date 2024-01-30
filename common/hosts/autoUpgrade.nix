6fad7ec3c8c2919e9e23e20b6d0f1f3282ba65d4
João Nogueira <joao.nogueira@eivee.io>
2024-01-30 14:19:09 +0100

    Added checks to avoid re-writing the conditional script in every build

--- common/hosts/autoUpgrade.nix
+++ common/hosts/autoUpgrade.nix
@@ -1,14 +1,28 @@
-{ config, inputs, pkgs, lib, ... }: {
+{ config, inputs, pkgs, lib, ... }:
+let
+  # attrSet to list;
+  registry = lib.pipe config.nix.registry [
+    (lib.attrValues)
+    (map (builtins.getAttr "flake"))
+  ];
+
+  # set 'outPath' to match the registry alias if 'inputs.self' is included;
+  self = inputs.self
+    // lib.optionalAttrs (builtins.any (flake: flake == inputs.self) registry) {
+      outPath = "flake:self";
+    };
+in {
   system.autoUpgrade = {
-    enable = inputs.self ? rev; # disable if dirty;
+    enable = self ? rev; # disable if dirty;
     dates = "hourly";
     flags = [ "--refresh" "--print-build-logs" ];
     flake = "github:c4f3z1n/nix-config";
   };
 
   systemd.services.nixos-upgrade = lib.mkIf config.system.autoUpgrade.enable {
+    path = [ pkgs.nix ];
     serviceConfig.ExecCondition = pkgs.writeTextFile {
-      name = "autoUpgrade-condition.nu";
+      name = "nixos-upgrade-condition.nu";
       executable = true;
       text = ''
         #!${lib.getExe pkgs.nushell}
@@ -17,7 +31,7 @@
           ^nix flake metadata $flake --refresh --json | from json
         }
         let upstream = metadata "${config.system.autoUpgrade.flake}"
-        let local = metadata "${inputs.self}"
+        let local = metadata "${self}"
         assert ($upstream.locked.narHash != $local.locked.narHash)
         assert ($upstream.lastModified > $local.lastModified)
       '';

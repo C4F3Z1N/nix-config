21915f00342018fc6c9a8aff7e65d6d9a775ca9c
João Nogueira <shires.waking0d@icloud.com>
2024-02-23 14:28:52 +0100

    Started using LibreOffice

--- common/home/joao/misc-gui.nix
+++ common/home/joao/misc-gui.nix
@@ -1,5 +1,11 @@
 { lib, osConfig, pkgs, ... }: {
-  home.packages = with pkgs; [ slack virt-manager virt-viewer vscode ];
+  home.packages = with pkgs; [
+    libreoffice
+    slack
+    virt-manager
+    virt-viewer
+    vscode
+  ];
 
   dconf.settings = with osConfig;
     lib.mkMerge [

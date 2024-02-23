e779e9ac382507ef0a8ea35a09efd1a949994575
João Nogueira <shires.waking0d@icloud.com>
2024-02-23 15:29:36 +0100

    Set Tmux as Gnome Console's shell

--- common/home/joao/misc-gui.nix
+++ common/home/joao/misc-gui.nix
@@ -1,4 +1,4 @@
-{ lib, osConfig, pkgs, ... }: {
+{ config, lib, osConfig, pkgs, ... }: {
   home.packages = with pkgs; [
     libreoffice
     slack
@@ -7,24 +7,28 @@
     vscode
   ];
 
-  dconf.settings = with osConfig;
-    lib.mkMerge [
-      (lib.mkIf services.xserver.desktopManager.gnome.enable {
-        "org/gnome/desktop/interface" = { color-scheme = "prefer-dark"; };
-        "org/gnome/mutter" = {
-          experimental-features = [ "scale-monitor-framebuffer" ];
-        };
-        "org/gnome/settings-daemon/plugins/color" = {
-          night-light-enabled = true;
-        };
-      })
-      (lib.mkIf virtualisation.libvirtd.enable {
-        "org/virt-manager/virt-manager/connections" = {
-          autoconnect = [ "qemu:///system" ];
-          uris = [ "qemu:///system" ];
-        };
-      })
-    ];
+  dconf.settings = lib.mkMerge [
+    (lib.mkIf osConfig.services.xserver.desktopManager.gnome.enable {
+      "org/gnome/desktop/interface" = { color-scheme = "prefer-dark"; };
+      "org/gnome/mutter" = {
+        experimental-features = [ "scale-monitor-framebuffer" ];
+      };
+      "org/gnome/settings-daemon/plugins/color" = {
+        night-light-enabled = true;
+      };
+    })
+    (lib.mkIf osConfig.virtualisation.libvirtd.enable {
+      "org/virt-manager/virt-manager/connections" = {
+        autoconnect = [ "qemu:///system" ];
+        uris = [ "qemu:///system" ];
+      };
+    })
+    (lib.mkIf config.programs.tmux.enable {
+      "/org/gnome/Console" = {
+        shell = [ lib.getExe config.programs.tmux.package ];
+      };
+    })
+  ];
 
   services.copyq.enable = true;
 }

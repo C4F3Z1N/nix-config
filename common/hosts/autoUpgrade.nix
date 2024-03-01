bc17d7aac11cfe784a335cda5afe74f8136a1226
João Nogueira <shires.waking0d@icloud.com>
2024-03-01 18:06:59 +0100

    Refactored ExecCondition to improve readability and resource usage

--- common/hosts/autoUpgrade.nix
+++ common/hosts/autoUpgrade.nix
@@ -14,20 +14,22 @@ in {
   };
 
   systemd.services.nixos-upgrade = lib.mkIf config.system.autoUpgrade.enable {
-    path = [ pkgs.nix ];
+    path = with pkgs; [ nix nushell ];
     serviceConfig.ExecCondition = pkgs.writeTextFile {
-      name = "nixos-upgrade-condition.nu";
       executable = true;
+      name = "nixos-upgrade-condition.nu";
       text = ''
-        #!${lib.getExe pkgs.nushell}
+        #!/usr/bin/env nu
         use std assert
-        def metadata [flake: string] {
-          ^nix flake metadata $flake --refresh --json | from json
+        def metadata [flake: string]: string -> record {
+          let output = do { ^nix flake metadata $flake --refresh --json } | complete
+          assert ($output.exit_code == 0) $output.stderr
+          return ($output.stdout | from json)
         }
         let upstream = metadata "${config.system.autoUpgrade.flake}"
         let local = metadata "${self}"
-        assert ($upstream.locked.narHash != $local.locked.narHash)
-        assert ($upstream.lastModified > $local.lastModified)
+        let latest = [ $upstream, $local ] | sort-by lastModified | last
+        assert ($upstream == $latest)
       '';
     };
   };

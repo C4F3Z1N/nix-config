14714994a063527181681d5fe4c544d50a02e7ff
João Nogueira <shires.waking0d@icloud.com>
2024-03-06 15:11:53 +0100

    Moved devshells section back to flake.nix

--- flake.nix
+++ flake.nix
@@ -57,8 +57,36 @@
       };
 
       perSystem = { inputs', pkgs, ... }: {
-        devshells.default =
-          import ./common/devshell.nix { inherit inputs' pkgs; };
+        devshells.default = {
+          packages = with (pkgs // inputs'.disko.packages); [
+            age
+            curl
+            disko
+            gitMinimal
+            gnupg
+            nixos-anywhere
+            nixos-install-tools
+            nushell
+            pinentry
+            sops
+            ssh-to-age
+            ssh-to-pgp
+            util-linux
+          ];
+
+          env = [
+            {
+              name = "NIX_CONFIG";
+              value =
+                "extra-experimental-features = flakes nix-command repl-flake";
+            }
+            {
+              name = "GNUPGHOME";
+              eval =
+                "$(printenv GNUPGHOME || find ~/.gnupg -maxdepth 0 || mktemp -d)";
+            }
+          ];
+        };
 
         treefmt.config = {
           projectRootFile = "flake.nix";

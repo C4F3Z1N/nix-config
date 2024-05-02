fc014524fbb199a5021fc0a498c73ffcb8069fa9
João Nogueira <22820003+c4f3z1n@users.noreply.github.com>
2024-05-02 17:52:30 +0200

    Added an overlay to nixpkgs

--- nixos/modules/nixpkgs.nix
+++ nixos/modules/nixpkgs.nix
@@ -1,6 +1,20 @@
-{
+{ inputs, lib, ... }: {
   nixpkgs = {
     config.allowUnfree = true;
     # config.contentAddressedByDefault = true;
+    overlays = [
+      # make packages from inputs available as pkgs.<name>;
+      (final: prev:
+        with prev.stdenv;
+        lib.pipe inputs [
+          (lib.mapAttrsToList (_:
+            { packages ? { }, legacyPackages ? { }, ... }:
+            if packages != { } then packages else legacyPackages))
+          (builtins.catAttrs hostPlatform.system)
+          (builtins.catAttrs "default")
+          (map (package: lib.nameValuePair (lib.getName package) package))
+          (builtins.listToAttrs)
+        ])
+    ];
   };
 }

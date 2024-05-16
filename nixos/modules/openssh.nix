1221758086b98683859aaf35bbe28f7cbbe422d7
João Nogueira <22820003+c4f3z1n@users.noreply.github.com>
2024-05-16 16:41:37 +0200

    Minor refactoring

--- nixos/modules/openssh.nix
+++ nixos/modules/openssh.nix
@@ -6,14 +6,14 @@ let
   hostPubKeys = lib.pipe "${inputs.secrets}/public-keys.json" [
     (lib.importJSON)
     (builtins.getAttr "hosts")
-    (builtins.mapAttrs (_: keys: filterSshKeys keys))
+    (builtins.mapAttrs (_: filterSshKeys))
   ];
   customKnownHosts = lib.pipe hostPubKeys [
     (lib.mapAttrsToList (name: keys:
       lib.pipe keys [ (builtins.attrValues) (map (key: "${name} ${key}")) ]))
     (lib.flatten)
     (lib.concatLines)
-    (pkgs.writeText "known_hosts")
+    (builtins.toFile "known_hosts")
   ];
 in {
   services.openssh = {
@@ -22,7 +22,7 @@ in {
     # use public keys to force 'HostKeyAgent' lookup;
     hostKeys = lib.mapAttrsToList (type: key: {
       inherit type;
-      path = pkgs.writeText "${hostName}-${type}.pub" key;
+      path = builtins.toFile "${hostName}-${type}.pub" key;
     }) hostPubKeys."${hostName}";
 
     settings = {

01f4d6a30f842ce33893484b12bf691731741ff0
João Nogueira <22820003+c4f3z1n@users.noreply.github.com>
2024-05-16 11:03:09 +0200

    Refactored host-gpg-agent as a full module

--- nixos/modules/gpg-agent.nix
+++ nixos/modules/gpg-agent.nix
@@ -1,47 +1,93 @@
 { config, lib, pkgs, ... }:
+with lib;
 let
-  GNUPGHOME = config.sops.gnupg.home;
-  SSH_AUTH_SOCK = "${GNUPGHOME}/S.gpg-agent.ssh";
+  cfg = config.services.host-gpg-agent;
+  envVars = {
+    GNUPGHOME = cfg.homedir;
+    SSH_AUTH_SOCK = "/run/host-gpg-agent/S.gpg-agent.ssh";
+  };
 in {
-  systemd = {
-    services.host-gpg-agent = rec {
-      environment = { inherit GNUPGHOME; };
-      after = requires;
-      requires = [ "host-gpg-agent-ssh.socket" "host-gpg-agent.socket" ];
-      serviceConfig = {
-        ExecReload = "${pkgs.gnupg}/bin/gpgconf --reload gpg-agent";
-        ExecStart = "${pkgs.gnupg}/bin/gpg-agent --supervised";
-      };
-      # unitConfig.RefuseManualStart = true;
+  options.services.host-gpg-agent = with types; {
+    enable = mkOption {
+      type = bool;
+      default = false;
     };
 
-    sockets = {
-      host-gpg-agent = {
-        partOf = [ "host-gpg-agent.service" ];
-        wantedBy = [ "sockets.target" ];
-        socketConfig = {
-          DirectoryMode = "0700";
-          FileDescriptorName = "std";
-          ListenStream = "${GNUPGHOME}/S.gpg-agent";
-          SocketMode = "0600";
+    homedir = mkOption {
+      type = path;
+      default = "/etc/gnupg";
+    };
+
+    package = mkOption {
+      type = package;
+      default = config.programs.gnupg.package;
+    };
+
+    verbose = mkOption {
+      type = bool;
+      default = true;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    nix = { inherit envVars; };
+
+    environment.etc."ssh/sshd_config.d/host-gpg-agent.conf".text =
+      "HostKeyAgent ${envVars.SSH_AUTH_SOCK}";
+
+    programs.ssh.extraConfig = ''
+      # The SSH agent protocol doesn't have support for changing TTYs; however we
+      # can simulate this with the `exec` feature of openssh (see ssh_config(5))
+      # that hooks a command to the shell currently running the ssh program.
+      Match host * exec "${cfg.package}/bin/gpg-connect-agent --quiet updatestartuptty /bye"
+    '';
+
+    services.openssh.extraConfig =
+      mkAfter "Include /etc/ssh/sshd_config.d/host-gpg-agent.conf";
+
+    systemd = {
+      services = {
+        host-gpg-agent = rec {
+          after = requires;
+          environment = envVars;
+          requires = [ "host-gpg-agent-ssh.socket" "host-gpg-agent.socket" ];
+          serviceConfig = {
+            ExecReload = "${cfg.package}/bin/gpgconf --reload gpg-agent";
+            ExecStart = "${cfg.package}/bin/gpg-agent --supervised"
+              + optionalString cfg.verbose " --verbose";
+          };
+          unitConfig.RefuseManualStart = true;
         };
       };
 
-      host-gpg-agent-ssh = {
-        partOf = [ "host-gpg-agent.service" ];
-        wantedBy = [ "sockets.target" ];
-        socketConfig = {
-          DirectoryMode = "0700";
-          FileDescriptorName = "ssh";
-          ListenStream = SSH_AUTH_SOCK;
-          Service = "host-gpg-agent.service";
-          SocketMode = "0600";
+      sockets = {
+        host-gpg-agent = {
+          partOf = [ "host-gpg-agent.service" ];
+          wantedBy = [ "sockets.target" ];
+          socketConfig = rec {
+            DirectoryMode = "0700";
+            ExecStartPre = "${pkgs.coreutils}/bin/rm -fv ${Symlinks}";
+            FileDescriptorName = "std";
+            ListenStream = "/run/host-gpg-agent/S.gpg-agent";
+            SocketMode = "0600";
+            Symlinks = "${cfg.homedir}/${builtins.baseNameOf ListenStream}";
+          };
+        };
+
+        host-gpg-agent-ssh = {
+          partOf = [ "host-gpg-agent.service" ];
+          wantedBy = [ "sockets.target" ];
+          socketConfig = rec {
+            DirectoryMode = "0700";
+            ExecStartPre = "${pkgs.coreutils}/bin/rm -fv ${Symlinks}";
+            FileDescriptorName = "ssh";
+            ListenStream = envVars.SSH_AUTH_SOCK;
+            Service = "host-gpg-agent.service";
+            SocketMode = "0600";
+            Symlinks = "${cfg.homedir}/${builtins.baseNameOf ListenStream}";
+          };
         };
       };
     };
   };
-
-  environment.sessionVariables = { inherit GNUPGHOME SSH_AUTH_SOCK; };
-
-  services.openssh.extraConfig = "HostKeyAgent ${SSH_AUTH_SOCK}";
 }

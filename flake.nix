8541bb5d2cc81fff08f73276a75aac714a454a91
João Nogueira <22820003+c4f3z1n@users.noreply.github.com>
2024-05-16 13:13:55 +0200

    Stopped using devshells and flake-utils

--- flake.nix
+++ flake.nix
@@ -6,11 +6,8 @@
     # so they're fetched from the local registry;
 
     # deduplication;
-    devshell.inputs.flake-utils.follows = "flake-utils";
-    devshell.inputs.nixpkgs.follows = "nixpkgs";
     disko.inputs.nixpkgs.follows = "nixpkgs";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
-    flake-utils.inputs.systems.follows = "systems";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix.inputs.flake-compat.follows = "flake-compat";
     nix.inputs.nixpkgs.follows = "nixpkgs";
@@ -24,11 +21,9 @@
   };
 
   outputs = inputs @ {
-    devshell,
     disko,
     flake-compat,
     flake-parts,
-    flake-utils,
     home-manager,
     impermanence,
     nix,
@@ -40,65 +35,22 @@
     ...
   }:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [devshell.flakeModule treefmt-nix.flakeModule];
+      imports = [treefmt-nix.flakeModule];
 
       flake = {lib, ...}: {
         homeConfigurations = import ./home-manager {inherit inputs lib;};
         nixosConfigurations = import ./nixos/hosts {inherit inputs lib;};
       };
 
-      perSystem = {
-        inputs',
-        pkgs,
-        ...
-      }: {
-        devshells.default = {
-          packages = with pkgs; [
-            age
-            curl
-            gitMinimal
-            gnupg
-            inputs'.disko.packages.disko
-            nixos-anywhere
-            nixos-install-tools
-            nushell
-            pinentry
-            sops
-            ssh-to-age
-            ssh-to-pgp
-            util-linux
-          ];
-
-          env = [
-            {
-              name = "GNUPGHOME";
-              eval = "$(printenv GNUPGHOME || find ~/.gnupg -maxdepth 0 || mktemp -d)";
-            }
-            {
-              name = "GPG_TTY";
-              eval = "$(tty)";
-            }
-            {
-              name = "NIX_CONFIG";
-              value = "extra-experimental-features = flakes nix-command repl-flake";
-            }
-            {
-              name = "SSH_AUTH_SOCK";
-              eval = "$(gpgconf --list-dirs agent-ssh-socket)";
-            }
-          ];
-        };
-
-        treefmt.config = {
-          # programs.alejandra.enable = true;
-          programs.nixfmt.enable = true;
-          programs.prettier.enable = true;
-          projectRootFile = "flake.nix";
-          settings.formatter = {
-            prettier.includes = ["*.lock"];
-            nixfmt.excludes = ["flake.nix"];
-            # TODO: use alejandra for flake.nix;
-          };
+      perSystem.treefmt.config = {
+        # programs.alejandra.enable = true;
+        programs.nixfmt.enable = true;
+        programs.prettier.enable = true;
+        projectRootFile = "flake.nix";
+        settings.formatter = {
+          prettier.includes = ["*.lock"];
+          nixfmt.excludes = ["flake.nix"];
+          # TODO: use alejandra for flake.nix;
         };
       };
 
--- shell.nix
+++
@@ -1,5 +0,0 @@
-if builtins ? getFlake then
-  (builtins.getFlake
-    (builtins.toString ./.)).devShells."${builtins.currentSystem}".default
-else
-  (import ./flake-compat.nix { }).shellNix
